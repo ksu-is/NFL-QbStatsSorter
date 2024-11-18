8682416da25a3ce43d1688e5eb17cbceefbaf34c
Nathan Baker <nbaker33@students.kennesaw.edu>
2024-11-18 15:23:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ pygame.display.set_caption('Pong Game')
 # Set up colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+
+# Set up the paddles and ball
+PADDLE_WIDTH, PADDLE_HEIGHT = 15, 100
+BALL_SIZE = 15
